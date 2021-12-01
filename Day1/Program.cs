1e5964ec2fee7def652c065e2140db7b21be3384
Daniel Snellman <daniel.a.snellman@outlook.com>
2021-12-01 11:17:08 +0100

    Day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day1
 {
@@ -6,7 +7,54 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\DanielSnellman\source\repos\AdventOfCode2021\Day1\input.txt");
+
+            part1(lines);
+            part2(lines);
+        }
+
+        public static void part1(string[] lines)
+        {
+            int lastDeepth = 0;
+            int increased = 0;
+            foreach (string line in lines)
+            {
+                int currentLine = int.Parse(line);
+                if (currentLine > lastDeepth && lastDeepth != 0)
+                {
+                    increased++;
+                }
+
+                lastDeepth = currentLine;
+            }
+
+            Console.WriteLine("Deepth increased " + increased + " times");
+
+        }
+
+        public static void part2(string[] lines)
+        {
+            List<int> Deepth = new List<int>();
+            int LastSum = 0;
+            int increased = 0;
+            foreach (string line in lines)
+            {
+                int currentLine = int.Parse(line);
+                if(Deepth.Count >= 2)
+                {
+                    int currentSum = currentLine + Deepth[0] + Deepth[1];
+                    if(LastSum < currentSum && LastSum != 0)
+                    {
+                        increased++;
+                    }
+                    LastSum = currentSum;
+                }
+
+                Deepth.Insert(0, currentLine);
+            }
+
+            Console.WriteLine("Deepth increased " + increased + " times");
+
         }
     }
 }
